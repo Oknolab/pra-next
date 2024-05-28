468d9f1f19c83e28c97f93480e2735039b946072
中北竜馬 <b1020183@fun.ac.jp>
2024-05-29 01:04:59 +0900

    UserListItemコンポーネントを作成

---
+++ src/components/UserListItem/UserListItem.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import UserListItem from './UserListItem';
+
+const meta = {
+  title: 'UserListItem',
+  component: UserListItem,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof UserListItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Sample: Story = {
+  args: {
+    user: {
+      id: '1',
+      userName: 'sample',
+      age: 20,
+    },
+    className: "w-[300px]"
+  },
+};
+
+export const LongName: Story = {
+  args: {
+    user: {
+      id: '2',
+      userName: 'This is a very long name',
+      age: 5,
+    },
+    className: "w-[300px]"
+  },
+};
---
+++ src/components/UserListItem/UserListItem.tsx
@@ -0,0 +1,24 @@
+import { UserType } from "@/types";
+import clsx from "clsx";
+
+type UserListItemProps = {
+  user: UserType;
+  className?: string;
+}
+
+const UserListItem = ({user, className}: UserListItemProps) => {
+  const _className = clsx(
+    'grid grid-cols-4 gap-2 border rounded-sm p-2',
+    className
+  );
+
+  return (
+    <li className={_className}>
+      <span className="truncate">{user.id}</span>
+      <span className="truncate col-span-2">{user.userName}</span>
+      <span className="truncate text-right">{user.age}歳</span>
+    </li>
+  );
+}
+
+export default UserListItem;
