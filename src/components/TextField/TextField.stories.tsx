e69bd94a76bdced51c49e7dafe08f512ed6ddd46
中北竜馬 <b1020183@fun.ac.jp>
2024-05-29 00:46:23 +0900

    TextFieldを作成

---
+++ src/components/TextField/TextField.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TextField from './TextField';
+
+const meta = {
+  title: 'TextField',
+  component: TextField,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof TextField>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Sample: Story = {
+  args: {
+    label: '名前',
+    placeholder: '名前を入力してください',
+    className: 'w-64 h-8',
+    name: 'name',
+    onChange: (name: string, value: string) => console.log(name, value),
+  },
+};
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+
+type TextFieldProps = {
+  label: string;
+  value?: string;
+  name?: string;
+  onChange?: (name: string, value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+const TextField = ({label, placeholder, className, name="", onChange, value=""}: TextFieldProps) => {
+  const _className = clsx(
+    'flex items-center space-x-2',
+    className
+  );
+
+  function _onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    
+    if (onChange) {
+      onChange(name, e.target.value);
+    }
+  }
+
+  return (
+    <label className={_className}>
+      <span>{label}</span>
+      <input
+        name={name}
+        type="text"
+        placeholder={placeholder}
+        className="border border-gray-300 rounded px-2 py-1"
+        value={value}
+        onChange={_onChange}
+      />
+    </label>
+  )
+}
+
+export default TextField;
